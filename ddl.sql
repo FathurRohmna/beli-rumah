db3e4dfeb9d40554931f9b9a097dd93c78be3a2b
Fathur Rohman <fr081938@gmail.com>
2024-12-18 23:44:46 +0700

    [feat] dll houses and user_house_transactions

--- ddl.sql
+++ ddl.sql
@@ -20,3 +20,30 @@ CREATE TABLE transactions (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+CREATE TYPE house_category AS ENUM ('apartment', 'villa', 'house', 'residentialComplex');
+
+CREATE TABLE houses (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    address TEXT NOT NULL,
+    category house_category NOT NULL,
+    size INTEGER NOT NULL,
+    unit_count INT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TYPE house_availability AS ENUM ('pending', 'sold', 'cancelled');
+
+CREATE TABLE user_house_transactions (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    house_id UUID NOT NULL REFERENCES houses(id) ON DELETE CASCADE,
+
+    transaction_status house_availability NOT NULL DEFAULT 'pending',
+    expired_at TIMESTAMP NOT NULL
+)
