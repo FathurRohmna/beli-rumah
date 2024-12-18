635d8f6963503cba67f7e8361dfe603315850035
Fathur Rohman <fr081938@gmail.com>
2024-12-19 00:38:17 +0700

    [feat] implement UserHouseTransactionController

--- controller/user_house_transaction_controller.go
+++ controller/user_house_transaction_controller.go
@@ -1,1 +1,8 @@
 package controller
+
+import "github.com/labstack/echo/v4"
+
+type IUserHouseTransactionController interface {
+	CancelTransactionHandler(c echo.Context) error
+	ConfirmTransactionHandler(c echo.Context) error
+}
--- controller/user_house_transaction_controller_impl.go
+++ controller/user_house_transaction_controller_impl.go
@@ -1,1 +1,40 @@
 package controller
+
+import (
+	"beli-tanah/service"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserHouseTransactionController struct {
+	UserHouseTransactionService service.IUserHouseTransactionService
+}
+
+func NewUserHouseTransactionController(userHouseTransactionService service.IUserHouseTransactionService) IUserHouseTransactionController {
+	return &UserHouseTransactionController{UserHouseTransactionService: userHouseTransactionService}
+}
+
+func (h *UserHouseTransactionController) CancelTransactionHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	transactionID := c.Param("transaction_id")
+
+	if err := h.UserHouseTransactionService.CancelTransaction(ctx, "", transactionID); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Transaction cancelled"})
+}
+
+func (h *UserHouseTransactionController) ConfirmTransactionHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	transactionID := c.Param("transaction_id")
+
+	if err := h.UserHouseTransactionService.ConfirmTransaction(ctx, "", transactionID); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Transaction confirmed as sold"})
+}
